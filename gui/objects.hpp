TeamWin/android_bootable_recovery

Add support to change TWRP Folder
Change-Id: Id3b3b46d492bf83d1d9d3f535c880ea9d15b4107

gui/objects.hpp
@@ -371,6 +371,7 @@ protected:
 	int uninstalltwrpsystemapp(std::string arg);
 	int repackimage(std::string arg);
 	int fixabrecoverybootloop(std::string arg);
+	int apply_CustomTWRPFolder(std::string arg);
 
 	int simulate;
 };

partitionmanager.cpp
@@ -1696,6 +1696,7 @@ void TWPartitionManager::Post_Decrypt(const string& Block_Device) {
 			DataManager::SetValue("tw_settings_path", "/data/media/0");
 			dat->UnMount(false);
 		}
+		DataManager::LoadTWRP_FolderInfo();
 		Update_System_Details();
 		Output_Partition(dat);
 		UnMount_Main_Partitions();

twrp.cpp
@@ -330,6 +330,7 @@ int main(int argc, char **argv) {
 
 	// Read the settings file
 	TWFunc::Update_Log_File();
+	DataManager::LoadTWRP_FolderInfo();
 	DataManager::ReadSettingsFile();
 	PageManager::LoadLanguage(DataManager::GetStrValue("tw_language"));
 	GUIConsole::Translate_Now();

sekaiacg/twrp_recovery

gui/objects.hpp
@@ -374,9 +374,11 @@ protected:
 	int enableadb(std::string arg);
 	int enablefastboot(std::string arg);
 	int changeterminal(std::string arg);
+	int applycustomtwrpfolder(std::string arg);
 #ifndef TW_EXCLUDE_NANO
 	int editfile(std::string arg);
 #endif
+
 	int simulate;
 };
 

partitionmanager.cpp
@@ -1770,6 +1770,7 @@ void TWPartitionManager::Post_Decrypt(const string& Block_Device) {
 			DataManager::SetValue("tw_settings_path", "/data/media/0");
 			dat->UnMount(false);
 		}
+		DataManager::LoadTWRPFolderInfo();
 		Update_System_Details();
 		Output_Partition(dat);
 		UnMount_Main_Partitions();

twrp.cpp
@@ -184,6 +184,7 @@ static void process_recovery_mode(twrpAdbBuFifo* adb_bu_fifo, bool skip_decrypti
 	if (crash_counter == 0)
 		TWFunc::Fixup_Time_On_Boot();
 
+	DataManager::LoadTWRPFolderInfo();
 	DataManager::ReadSettingsFile();
 
 	// Run any outstanding OpenRecoveryScript