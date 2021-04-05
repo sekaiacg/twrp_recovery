76224bde3d4d2bd5580dfab5141c113b8d64d87e
nebrassy <nebras30@gmail.com>
2021-04-05 11:52:44 +0200

    Support AIDL Vibrator
    co-authored by: SebaUbuntu <barezzisebastiano@gmail.com>
    
    Change-Id: Ifaa0af4cb0fe0dee7d2a9ad4c58a5ede5ce46d59

--- minuitwrp/events.cpp
+++ minuitwrp/events.cpp
@@ -32,6 +32,13 @@
 #include <android/hardware/vibrator/1.2/IVibrator.h>
 #endif
 
+#ifdef USE_QTI_AIDL_HAPTICS
+#include <aidl/android/hardware/vibrator/IVibrator.h>
+#include <android/binder_manager.h>
+using ::aidl::android::hardware::vibrator::IVibrator;
+static const std::string kVibratorInstance = std::string(IVibrator::descriptor) + "/default";
+#endif
+
 #include "common.h"
 
 #include "minuitwrp/minui.h"
@@ -139,6 +146,11 @@ int vibrate(int timeout_ms)
     android::sp<android::hardware::vibrator::V1_2::IVibrator> vib = android::hardware::vibrator::V1_2::IVibrator::getService();
     if (vib != nullptr) {
         vib->on((uint32_t)timeout_ms);
+    }
+#elif defined(USE_QTI_AIDL_HAPTICS)
+    std::shared_ptr<IVibrator> vib = IVibrator::fromBinder(ndk::SpAIBinder(AServiceManager_getService(kVibratorInstance.c_str())));
+    if (vib != nullptr) {
+        vib->on((uint32_t)timeout_ms, nullptr);
     }
 #else
     if (std::ifstream(LEDS_HAPTICS_ACTIVATE_FILE).good()) {
