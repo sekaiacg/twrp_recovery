4076a67cd3b101999a2a852979f95c44425d36d0
Captain Throwback <captainthrowback@hotmail.com>
2021-12-10 15:31:48 -0500

    postrecoveryboot: rename script to reflect recovery mode
    Change-Id: Ifb9d64a2bec118592523682749093bf894ec2cbe

--- twrp.cpp
+++ twrp.cpp
@@ -201,7 +201,7 @@ static void process_recovery_mode(twrpAdbBuFifo* adb_bu_fifo, bool skip_decrypti
 
 	// Check for and run startup script if script exists
 	TWFunc::check_and_run_script("/system/bin/runatboot.sh", "boot");
-	TWFunc::check_and_run_script("/system/bin/postrecoveryboot.sh", "boot");
+	TWFunc::check_and_run_script("/system/bin/postrecoveryboot.sh", "recovery");
 
 #ifdef TW_INCLUDE_INJECTTWRP
 	// Back up TWRP Ramdisk if needed:
