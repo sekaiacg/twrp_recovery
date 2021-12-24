7f582fbef02f20f761a206e28ab5f2f9e25b8f0d
sekaiacg <sekaiacg@gmail.com>
2021-12-24 09:28:41 +0800

    custom: Daylight savings time (DST) is not used by default
    Signed-off-by: sekaiacg <sekaiacg@gmail.com>

--- data.cpp
+++ data.cpp
@@ -764,7 +764,7 @@ void DataManager::SetDefaultValues()
 	mPersist.SetValue(TW_SDPART_FILE_SYSTEM, "ext3");
 	mPersist.SetValue(TW_TIME_ZONE_GUISEL, "CST6;CDT,M3.2.0,M11.1.0");
 	mPersist.SetValue(TW_TIME_ZONE_GUIOFFSET, "0");
-	mPersist.SetValue(TW_TIME_ZONE_GUIDST, "1");
+	mPersist.SetValue(TW_TIME_ZONE_GUIDST, "0");
 	mPersist.SetValue(TW_AUTO_REFLASHTWRP_VAR, "0");
 #ifdef TW_NO_FLASH_CURRENT_TWRP
 	mConst.SetValue("tw_no_flash_current_twrp", "1");
