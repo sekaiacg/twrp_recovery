TeamWin/android_bootable_recovery

decryption: solve specific decryption problems on some devices
Change-Id: I832c16da3d45888299fd9f96888ed8a85e5d48c9

Android.mk
@@ -183,6 +183,10 @@ ifeq ($(TW_NO_FLASH_CURRENT_TWRP),true)
     LOCAL_CFLAGS += -DTW_NO_FLASH_CURRENT_TWRP
 endif
 
+ifeq ($(TW_FIX_DECRYPTION_ON_DATA_MEDIA),true)
+    LOCAL_CFLAGS += -DTW_FIX_DECRYPTION_ON_DATA_MEDIA
+endif
+
 LOCAL_MODULE_PATH := $(TARGET_RECOVERY_ROOT_OUT)/system/bin
 
 ifeq ($(TARGET_RECOVERY_TWRP_LIB),)

sekaiacg/twrp_recovery

Android.mk
@@ -183,6 +183,10 @@ ifeq ($(TW_NO_FLASH_CURRENT_TWRP),true)
     LOCAL_CFLAGS += -DTW_NO_FLASH_CURRENT_TWRP
 endif
 
+ifeq ($(TW_PREPARE_DATA_MEDIA_EARLY),true)
+    LOCAL_CFLAGS += -DTW_PREPARE_DATA_MEDIA_EARLY
+endif
+
 LOCAL_MODULE_PATH := $(TARGET_RECOVERY_ROOT_OUT)/system/bin
 
 ifeq ($(TARGET_RECOVERY_TWRP_LIB),)