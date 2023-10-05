TeamWin/android_bootable_recovery

partition: Perform BLKDISCARD on the block device before flashing image
Change-Id: Ifb13d47693edfc96369053199ac0b5d81910f104

Android.mk
@@ -425,6 +425,9 @@ endif
 ifneq ($(TARGET_OTA_ASSERT_DEVICE),)
     LOCAL_CFLAGS += -DTARGET_OTA_ASSERT_DEVICE='"$(TARGET_OTA_ASSERT_DEVICE)"'
 endif
+ifeq ($(TW_BLKDISCARD), true)
+    LOCAL_CFLAGS += -DTW_BLKDISCARD
+endif
 
 LOCAL_C_INCLUDES += system/vold \
 

sekaiacg/twrp_recovery

Android.mk
@@ -438,6 +438,9 @@ endif
 ifeq ($(TW_INCLUDE_FASTBOOTD), true)
     LOCAL_CFLAGS += -DTW_INCLUDE_FASTBOOTD
 endif
+ifeq ($(TW_ENABLE_BLKDISCARD), true)
+    LOCAL_CFLAGS += -DTW_ENABLE_BLKDISCARD
+endif
 
 LOCAL_C_INCLUDES += system/vold \
 